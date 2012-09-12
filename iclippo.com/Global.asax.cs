591a157947be9000109131fa15bb3c5a8c7020a5
Hasitha Guruge <gurugehk@gmail.com>
2012-09-12 11:25:39 +0530

    Initial Commit By Hasitha

---
+++ iclippo.com/Global.asax.cs
@@ -0,0 +1,100 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Optimization;
+using System.Web.Routing;
+using System.Web.Security;
+using System.Web.SessionState;
+using iclippo.com;
+using System.Web.Routing;
+
+namespace iclippo.com
+{
+    public class Global : HttpApplication
+    {
+        void Application_Start(object sender, EventArgs e)
+        {
+            // Code that runs on application startup
+            BundleConfig.RegisterBundles(BundleTable.Bundles);
+
+            // Add Routes.
+            RegisterRoutes(RouteTable.Routes);
+
+        }
+
+        void Application_End(object sender, EventArgs e)
+        {
+            //  Code that runs on application shutdown
+
+        }
+
+        void Application_Error(object sender, EventArgs e)
+        {
+            // Code that runs when an unhandled error occurs
+
+            // Get last error from the server
+            Exception exc = Server.GetLastError();
+
+            if (exc is HttpUnhandledException)
+            {
+                if (exc.InnerException != null)
+                {
+                    exc = new Exception(exc.InnerException.Message);
+                    Server.Transfer("ErrorPage.aspx?handler=Application_Error%20-%20Global.asax",
+                        true);
+                }
+            }
+        }
+
+        void Session_Start(object sender, EventArgs e)
+        {
+            // Code that runs when a new session is started
+
+        }
+
+        void Session_End(object sender, EventArgs e)
+        {
+            // Code that runs when a session ends.
+            // Note: The Session_End event is raised only when the sessionstate mode
+            // is set to InProc in the Web.config file. If session mode is set to StateServer
+            // or SQLServer, the event is not raised.
+
+        }
+
+        void RegisterRoutes(RouteCollection routes)
+        {
+            routes.MapPageRoute(
+                "HomeRoute",
+                "Home",
+                "~/Default.aspx"
+            );
+            routes.MapPageRoute(
+                "AboutRoute",
+                "About",
+                "~/About.aspx"
+            );
+            routes.MapPageRoute(
+                "ContactRoute",
+                "Contact",
+                "~/Contact.aspx"
+            );
+            routes.MapPageRoute(
+                "ProductListRoute",
+                "ProductList",
+                "~/ProductList.aspx"
+            );
+
+            routes.MapPageRoute(
+                "ProductsByCategoryRoute",
+                "ProductList/{categoryName}",
+                "~/ProductList.aspx"
+            );
+            routes.MapPageRoute(
+                "ProductByNameRoute",
+                "Product/{productName}",
+                "~/ProductDetails.aspx"
+            );
+        }
+    }
+}
---
+++ iclippo.com/Scripts/_references.js
@@ -0,0 +1,4 @@
+﻿/// <reference path="modernizr-2.6.2.js" />
+/// <reference path="jquery-1.8.1.js" />
+/// <reference path="jquery-ui-1.8.11.js" />
+/// <reference path="modernizr-2.0.6-development-only.js" />
